481704af72d5b370d9b9e597bb61aca9ad6c0f11
kaio henrique <suporte@seomidia.com.br>
2021-04-24 21:08:11 -0300

    ajuste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -69,45 +69,45 @@
 @endforeach
 
 
-  <div class="site-section bg-light">
-    <div class="container">
-      <div class="row">
-        <div class="title-section text-center col-12">
-          <h2 class="text-uppercase">Serviços destaque</h2>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12 block-3 products-wrap">
-          <div class="nonloop-block-3 owl-carousel">
+{{--  <div class="site-section bg-light">--}}
+{{--    <div class="container">--}}
+{{--      <div class="row">--}}
+{{--        <div class="title-section text-center col-12">--}}
+{{--          <h2 class="text-uppercase">Serviços destaque</h2>--}}
+{{--        </div>--}}
+{{--      </div>--}}
+{{--      <div class="row">--}}
+{{--        <div class="col-md-12 block-3 products-wrap">--}}
+{{--          <div class="nonloop-block-3 owl-carousel">--}}
 
-              @foreach($produtos as $produto)
+{{--              @foreach($produtos as $produto)--}}
 
-                  <div class="text-center item mb-4">
-                      <a href="produto/{{$produto->slug}}">
-                            <h3 class="text-dark">{{$produto->name}}</h3>
-                          <p class="price">
-                              @if($produto->sale == '')
-                                  R$ {{$produto->price}}
-                              @else
-                                  <del>R$ {{$produto->price}}</del>
-                                  @endif
-                                  @if($produto->sale != '') &mdash; R$ {{$produto->sale}} @endif
-                          </p>
-                            </a>
-                      <form action="/carrinho/add" name="{{$produto->slug}}" method="post">
-                          @csrf
-                          <input type="hidden" name="product_id" value="{{$produto->id}}">
-                          <button class="btn btn-outline-secondary add_cart" type="submit">Add Carrinho</button>
-                      </form>
-                </div>
+{{--                  <div class="text-center item mb-4">--}}
+{{--                      <a href="produto/{{$produto->slug}}">--}}
+{{--                            <h3 class="text-dark">{{$produto->name}}</h3>--}}
+{{--                          <p class="price">--}}
+{{--                              @if($produto->sale == '')--}}
+{{--                                  R$ {{$produto->price}}--}}
+{{--                              @else--}}
+{{--                                  <del>R$ {{$produto->price}}</del>--}}
+{{--                                  @endif--}}
+{{--                                  @if($produto->sale != '') &mdash; R$ {{$produto->sale}} @endif--}}
+{{--                          </p>--}}
+{{--                            </a>--}}
+{{--                      <form action="/carrinho/add" name="{{$produto->slug}}" method="post">--}}
+{{--                          @csrf--}}
+{{--                          <input type="hidden" name="product_id" value="{{$produto->id}}">--}}
+{{--                          <button class="btn btn-outline-secondary add_cart" type="submit">Add Carrinho</button>--}}
+{{--                      </form>--}}
+{{--                </div>--}}
 
-              @endforeach
+{{--              @endforeach--}}
 
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+{{--          </div>--}}
+{{--        </div>--}}
+{{--      </div>--}}
+{{--    </div>--}}
+{{--  </div>--}}
 
   <div class="site-section bg-secondary" style="margin: 0px;padding: 0px;background-color:#fff!important">
       <iframe width="100%" height="400" id="gmap_canvas" src="{{setting('site.mapa')}}" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
